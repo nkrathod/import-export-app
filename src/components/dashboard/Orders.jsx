5dff85cd2c235311f6fdaf68da3c98f69b168912
Nitesh Rathod <niteshrathod15205@gmail.com>
2024-02-16 17:50:49 +0530

    order updated

--- src/components/dashboard/Orders.jsx
+++ src/components/dashboard/Orders.jsx
@@ -1,11 +1,12 @@
-import * as React from 'react';
-import Link from '@mui/material/Link';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Title from './Title';
+import * as React from "react";
+import Link from "@mui/material/Link";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Title from "./Title";
+import axios from "axios";
 
 // Generate Order Data
 function createData(id, date, name, shipTo, paymentMethod, amount) {
@@ -15,36 +16,43 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
 const rows = [
   createData(
     0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
+    "16 Mar, 2019",
+    "Elvis Presley",
+    "Tupelo, MS",
+    "VISA ⠀•••• 3719",
+    312.44
   ),
   createData(
     1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
+    "16 Mar, 2019",
+    "Paul McCartney",
+    "London, UK",
+    "VISA ⠀•••• 2574",
+    866.99
   ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+  createData(
+    2,
+    "16 Mar, 2019",
+    "Tom Scholz",
+    "Boston, MA",
+    "MC ⠀•••• 1253",
+    100.81
+  ),
   createData(
     3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
+    "16 Mar, 2019",
+    "Michael Jackson",
+    "Gary, IN",
+    "AMEX ⠀•••• 2000",
+    654.39
   ),
   createData(
     4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
+    "15 Mar, 2019",
+    "Bruce Springsteen",
+    "Long Branch, NJ",
+    "VISA ⠀•••• 5919",
+    212.79
   ),
 ];
 
@@ -53,6 +61,47 @@ function preventDefault(event) {
 }
 
 export default function Orders() {
+  const [data, setData] = React.useState([]);
+
+  const str = "2022-04-26";
+
+  const date = new Date(str);
+
+  // ✅ Get timestamp in Milliseconds
+  const timestamp = date.getTime();
+  console.log(timestamp); // 👉️ 1650931200000
+
+  // ✅ If you need to convert milliseconds to seconds
+  // divide by 1000
+  const unixTimestamp = Math.floor(date.getTime() / 1000);
+  console.log(unixTimestamp); // 👉️ 1650931200
+
+  console.log(
+    new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(timestamp)
+  );
+
+  React.useEffect(() => {
+    if (data.length === 0) {
+      axios
+        .get("http://localhost:3000/import-data")
+        .then((res) => {
+          if (res.data.length > 0) {
+            console.log(res.data);
+            setData(res.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, []);
   return (
     <React.Fragment>
       <Title>Recent Orders</Title>
