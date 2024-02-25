1c0aae4ad250f915b8bb87ec2d77670390dc47de
Nitesh Rathod <niteshrathod15205@gmail.com>
2024-02-26 01:06:09 +0530

    chart updated

--- src/components/dashboard/Chart.jsx
+++ src/components/dashboard/Chart.jsx
@@ -2,24 +2,7 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import { LineChart, axisClasses } from "@mui/x-charts";
 import Title from "./Title";
-import { getMonths, chartDataValue } from "../../helpers";
-
-// Generate Sales Data
-function createData(time, amount) {
-  return { time, amount: amount ?? null };
-}
-
-const data = [
-  createData("00:00", 0),
-  createData("03:00", 300),
-  createData("06:00", 600),
-  createData("09:00", 800),
-  createData("12:00", 1500),
-  createData("15:00", 2000),
-  createData("18:00", 2400),
-  createData("21:00", 2400),
-  createData("24:00"),
-];
+import { chartDataValue } from "../../helpers";
 
 const chartDataFormat = [
   { month: "Jan", amount: 0 },
